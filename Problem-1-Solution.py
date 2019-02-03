09728546f048dcfd88275d45ae47d77f4a3bc67b
kenjir <35819962+cliffordrichardson@users.noreply.github.com>
2019-02-02 20:03:36 -0800

    I'm dumb

--- Problem-1-Solution.py
+++ Problem-1-Solution.py
@@ -4,11 +4,16 @@ multiples_of_five = set()
 print("Enter the number for up until we want to find all multiples of three and five")
 user_input = int(input())
 
-for x in range(user_input):
-    if not x % 3:
-        multiples_of_three.add(x)
-    if not x % 5:
-        multiples_of_five.add(x)
+x = 0
+counter = 0
+
+for x in range(0, user_input, 3):
+
+    multiples_of_three.add(x)
+    if x + (2 * counter) < user_input:
+        multiples_of_five.add(x + int((2 * counter)))
+        counter += 1
+
 
 whole_set = multiples_of_three | multiples_of_five
 sum = 0
